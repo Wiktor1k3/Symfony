9c565b79674d99902bd0da98225f7542c3948106
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2023-08-01 22:22:07 +0200

    Add files via upload

---
+++ symfony_6/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/admin' => [[['_route' => 'admin', '_controller' => 'App\\Controller\\Admin\\DashboardController::index'], null, null, null, false, false, null]],
+        '/sign-in' => [[['_route' => 'app_sign_in', '_controller' => 'App\\Controller\\AuthController::app_sign_in'], null, null, null, false, false, null]],
+        '/sign-out' => [[['_route' => 'app_sign_out', '_controller' => 'App\\Controller\\AuthController::app_sign_out'], null, null, null, false, false, null]],
+        '/register' => [[['_route' => 'app_register', '_controller' => 'App\\Controller\\RegistrationController::register'], null, null, null, false, false, null]],
+        '/verify/email' => [[['_route' => 'app_verify_email', '_controller' => 'App\\Controller\\RegistrationController::verifyUserEmail'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
